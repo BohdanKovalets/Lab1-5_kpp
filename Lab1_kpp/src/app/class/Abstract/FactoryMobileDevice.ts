f12b131e87485cf50b1c1f59442a0ecc0f420c03
BohdanKovalets <kovaletss.bogdan@gmail.com>
2025-04-03 15:13:40 +0300

    minimal_changes_lab3

--- Lab1_kpp/src/app/class/Abstract/FactoryMobileDevice.ts
+++ Lab1_kpp/src/app/class/Abstract/FactoryMobileDevice.ts
@@ -5,16 +5,16 @@ import { NameMobileDeviceMaps } from "./NameMobileDevice";
 
 export class FactoryMobileDevice {
     public static getMobileDevice(
-        name: string,
+        type: string,
         brand: string,
         batteryCapacity: number,
         weight: number,
         extraParam: any // Останній параметр буде або cameraResolution, або screenSize
     ): MobileDevice {
-        if (name === NameMobileDeviceMaps["smartphone"]) {
-            return new Smartphone(name, brand, batteryCapacity, weight, extraParam); 
-        } else if (name === NameMobileDeviceMaps["tablet"]) {
-            return new Tablet(name, brand, batteryCapacity, weight, extraParam); 
+        if (type === NameMobileDeviceMaps["smartphone"]) {
+            return new Smartphone(type, brand, batteryCapacity, weight, extraParam); 
+        } else if (type === NameMobileDeviceMaps["tablet"]) {
+            return new Tablet(type, brand, batteryCapacity, weight, extraParam); 
         } else {
             throw new Error("Невідомий тип пристрою");
         }
